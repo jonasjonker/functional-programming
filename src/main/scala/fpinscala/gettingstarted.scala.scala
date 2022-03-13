2a4677cbb070bfd6b3d454716b246a001a141814
Jonas Jonker <jonasjonker@gmail.com>
2022-03-13 11:28:58 +0100

    higher order function

--- src/main/scala/fpinscala/gettingstarted.scala.scala
+++ src/main/scala/fpinscala/gettingstarted.scala.scala
@@ -28,23 +28,24 @@ object MyModule {
     println("%12s => %d".format("%s(%d)".format(name, n), f(n)))
   }
 
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean =
+      if (n >= as.length - 1) true
+      else if (ordered(as(n), as(n + 1))) go(n + 1)
+      else false
+
+    go(0)
+  }
+
+
   def main(args: Array[String]): Unit = {
     debug("abs", -42, abs)
     debug("factorial", 7, factorial)
     debug("fibonaci", 42, fibonacci)
+    println(isSorted(Array(1,2,3), (x: Int, y: Int) => x <= y))
+    println(isSorted(Array(2,2,3), (x: Int, y: Int) => x <= y))
+    println(isSorted(Array(3,2,3), (x: Int, y: Int) => x <= y))
   }
 
-  object PolymorphicFunctions {
-
-    def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
-      @annotation.tailrec
-      def go(n: Int): Boolean = {
-        if (n < as.length - 1 && ordered(as[n], as[n + 1])) go(n + 1)
-        else ordered(as[n], as[n + 1])
-      }
-
-      go(0)
-    }
-
-  }
 }
